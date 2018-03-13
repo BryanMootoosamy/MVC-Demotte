59e7744c1b4820e2e2551c8776ec9673477c3103
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-03-13 16:07:13 +0100

    maj

--- view/presentation.php
+++ view/presentation.php
@@ -1,1 +1,14 @@
-<p>coucou</p>
+<div class="presentation">
+    <section class="presentationTop">
+        <div class="borderTop">
+            <ul>
+                <li>Antiquité</li>
+                <li>&#9679; Décoration</li>
+                <li>&#9679; Aménagement d'intérieur</li>
+            </ul>
+        </div>
+        <div class="borderTop2">
+
+        </div>
+    </section>
+</div>
