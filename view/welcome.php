e0f16f70af03c14c4fa5b2490efe0fb02613a0c3
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-03-13 15:42:07 +0100

    structure

--- view/welcome.php
+++ view/welcome.php
@@ -1,9 +1,15 @@
 <div class="welcomePage">
+    <div class="borderTop">
+
+    </div>
     <div class="welcomeScroll">
         <section class="header">
             <div class="top">
                 <img class="vcard" src="assets/pics/vcard.png" alt="oeil">
-                <h1 class="name">Didier Motte</h1>
+                <div class="name">
+                    <h1 class="firstname">Didier</h1>
+                    <h1 class="lastname">Motte</h1>
+                </div>
             </div>
             <div class="proList">
                 <ul>
@@ -19,6 +25,18 @@
                 <a class="a" href="index.php?action=presentation">Présentation</a>
                 <a class="b" href="index.php?action=stock">Le stock</a>
                 <a class="c" href="index.php?action=contact">Contact</a>
+            </div>
+        </section>
+        <section class="welcomeStock">
+            <div class="stockTitle">
+                <h1>Le Stock</h1>
+            </div>
+            <div class="research">
+                <form class="research" action="index.php" method="post">
+                    <h4>Fouiller le stock</h4>
+                    <input type="text" name="search" placeholder="Exemple: table..." value="">
+                    <button type="send" class="researchButton" name="searchButton">Rechercher</button>
+                </form>
             </div>
         </section>
     </div>
