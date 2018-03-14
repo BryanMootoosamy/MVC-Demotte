e7e0dbe374901fbc9b4d6807d44b281650e55296
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-03-14 15:04:18 +0100

    update

---
+++ view/contact.php
@@ -0,0 +1,41 @@
+<div class="contact">
+    <section class="contactTop">
+        <div class="borderTop">
+            <ul>
+                <li>Antiquité</li>
+                <li>&#9679; Décoration</li>
+                <li>&#9679; Aménagement d'intérieur</li>
+            </ul>
+        </div>
+        <div class="borderTop2">
+            <div class="presentationName">
+                <img src="assets/pics/vcard.png" alt="oeil">
+                <div class="name">
+                    <h1 class="firstname">Didier</h1>
+                    <h1 class="lastname">Motte</h1>
+                </div>
+                <div class="presentationLink">
+                    <a class="a" href="index.php?action=presentation">Présentation</a>
+                    <a class="b" href="index.php?action=stock">Le stock</a>
+                    <a class="c" href="index.php?action=contact">Contact</a>
+                </div>
+                <div class="phoneNumber">
+                    <a class="number" href="tel:+32 495/20.59.34">&#9742; +32 495/20.59.34</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="main">
+        <div class="title">
+            <h1>Contact</h1>
+        </div>
+        <div class="magInfo">
+            <h1>Le Magasin</h1>
+            <p>Le magasin est ouvert le samedi matin de 10h00 à 12h30 ou sur rendez-vous.</p>
+        </div>
+        <div class="mail">
+            <h1>Didier Motte</h1>
+            <a href="mail:dmotte@skynet.be">dmotte@skynet.be</a>
+        </div>
+    </section>
+</div>
--- view/presentation.php
+++ view/presentation.php
@@ -19,7 +19,59 @@
                     <a class="b" href="index.php?action=stock">Le stock</a>
                     <a class="c" href="index.php?action=contact">Contact</a>
                 </div>
+                <div class="phoneNumber">
+                    <a class="number" href="tel:+32 495/20.59.34">&#9742; +32 495/20.59.34</a>
+                </div>
             </div>
         </div>
+    </section>
+    <section class="main">
+        <h1 class="presentationMotte">DIDIER MOTTE</h1>
+        <img src="assets/pics/didier.png" alt="Didier Motte">
+        <h2>Les meubles ont une histoire, authentique.</h2>
+        <p class="desc1">Toujours à la recherche de l’extra-ordinaire, de l’insolite,
+        fasciné par la « petite » histoire du monde, celle des métiers d’autrefois,
+        Didier Motte fait partie de ces quelques connaisseurs, brocanteurs ou décorateurs qui,
+        à travers leur regard et leur toucher, savent percevoir l’histoire des objets anciens pour
+        leur donner une nouvelle légitimité esthétique et fonctionnelle.</p>
+        <p class="desc2">Je fais essentiellement des anciens meubles de commerce,
+        des intérieurs complets de vieux magasins, bistrots, pharmacies…
+        J’ai un entrepôt à Luttre, à Nivelles et à Haerelbeek .
+        Si vous êtes dans le coin, vous pouvez venir jeter un coup d’oeil
+        ( appelez-moi auparavant au 0495 205 934 ).  À bientôt! – Didier.</p>
+        <h1>Services</h1>
+        <p>Location de meubles et accessoires pour tournage.</p>
+        <h1>Livraison</h1>
+        <p>Livraison dans toute l’Europe & le monde.</p>
+        <h1>Ils ont fait confiance</h1>
+        <ul class="confiance">
+            <li>L’OSEILLE : ( Paris ) Restaurant .</li>
+            <li>LE BISTROT DE LA GALETTE : ( Paris ) Restaurant .</li>
+            <li>BARBER SHOES : ( Ixelles ) Barbier et chausseur .</li>
+            <li>EAUX VIVES : ( Genève ) Pharmacie</li>
+            <li>BIG MOUSTACHE : ( Paris ) Barbier</li>
+            <li>BALTHAZAR : ( Bruxelles ) Fauteuils design .</li>
+            <li><a class="buttonList" href="http://zahia.be/">Zahia</a>: ( Anvers , Gand , Brugge , Hasselt  ) Bijoux .</li>
+            <li><a class="buttonList" href="http://www.vino-events.com/">Vinopres</a>: ( Bruxelles ) Publications sur le monde du vin .</li>
+            <li><a class="buttonList" href="http://le-relais-du-triporteur.be/#home">Le relais du Triporteur</a> : ( Bruxelles ) Epicerie et restaurant Bio .</li>
+            <li><a class="buttonList" href="http://www.superfillesdutram.com/">Les super filles du tram</a>: ( Bruxelles ) Restaurant .</li>
+            <li><a class="buttonList" href="http://www.intaglio.fr/">Intaglio</a>: ( Paris ) Imprimeur , graveur , papier de créations .</li>
+            <li><a class="buttonList" href="http://www.villa-alice.be/">Villa Alice</a> : ( Westande-bad ) Restaurant .</li>
+            <li>MAMMUTI : ( Ile de Ré ) Galerie , appartements d’hôtes .</li>
+            <li>ETABLISSEMENTS MAX BLOCH : ( Bruxelles ) Grossiste en tissus .</li>
+            <li>RAW MATERIALS : ( Amsterdam ) Décoration .</li>
+            <li>MONS 2015 : Restaurant La Maison Folie</li>
+            <li>LA MERCERIE : ( Ixelles ) Salon de Thé et Dégustation</li>
+            <li>MY ITALIAN FRIENDS : ( Waterloo ) Restaurant et design italien</li>
+            <li>...</li>
+        </ul>
+        <footer class="presentationFooter">
+            <div class="ligne1">
+                <p class="lignea">&#9400; 2018 Didier Motte +32 495 20 59 34 - <a class="footerLink" href="mail:dmotte@skynet.be">dmotte@skynet.be</a> - <a class="footerLink" href="index.php?action=contact">Plan D'accès</a></p>
+                <p>Décorateur, Antiquités, Aménagement d'intérieurs, Belgique</p>
+                <p>Restez à jour via: <a class="ligneb" href="https://www.facebook.com/antiquites.decoration">Facebook</a> & <a class="ligneb" href="https://www.instagram.com/DidierMotte/">Instagram</a></p>
+                <p>Réalisé dans le cadre de la formation BeCode</p>
+            </div>
+        </footer>
     </section>
 </div>
